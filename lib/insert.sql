c6b8ff60994a2f9eb2517becad64e454d2c92568
Lauren <halpertln@gmail.com>
2023-05-08 12:39:20 -0600

    complete lab fixed

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "brown", "goofy", true);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "M", "black", "growly", true);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "brown", "sweet", true);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "brown", "growly", true);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 3, "F", "black", "goofy", true);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 10, "M", "black", "growly", true);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 13, "F", "white", "sweet", true);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, "F", "white", "goofy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "brown", "goofy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "M", "black", "growly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "brown", "sweet", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "brown", "growly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 3, "F", "black", "goofy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 10, "M", "black", "growly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 13, "F", "white", "sweet", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, "F", "white", "goofy", 1);
